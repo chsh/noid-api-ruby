dbe7dae8bec55429effc1d687d73cd7b52579a48
CHIKURA Shinsaku <shinsaku@chikura.me>
2023-12-26 10:29:11 +0900

    Initial commit

---
+++ lib/noid/api/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Noid
+  module Api
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/noid/api_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Noid::Api do
+  it "has a version number" do
+    expect(Noid::Api::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
